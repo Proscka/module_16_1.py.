a77948208a74a803e077b169a171903eec3272ec
Proscka <proskinaviktoria861@gmail.com>
2024-12-01 20:26:25 +0300

    Add files via upload

---
+++ module_16_1.py/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome()-> str:
+    return {f"Главная страница"}
+
+@app.get("/user/admin")
+async def administrator()-> str:
+    return {f"Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def user_number(user_id:int):
+    return (f"Вы вошли как пользователь №{user_id}")
+
+@app.get('/user')
+async def user_info(username: str,age:int):
+        return(f"Информация о пользователе. Имя:{username}, возраст:{age}")
